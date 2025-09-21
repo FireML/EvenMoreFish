1aeb6980a07cd70f1ef99c3838d34d24645aaf26
FireML <fireml@firedev.uk>
2025-09-21 20:46:25 +0100

    Add various different GUI types

---
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/types/BasicConfigGui.java
@@ -0,0 +1,25 @@
+package com.oheers.fish.gui.types;
+
+import com.oheers.fish.gui.base.BaseConfigGui;
+import dev.dejvokep.boostedyaml.block.implementation.Section;
+import dev.triumphteam.gui.guis.Gui;
+import org.bukkit.entity.HumanEntity;
+import org.jetbrains.annotations.NotNull;
+
+public class BasicConfigGui extends BaseConfigGui<Gui> {
+
+    public BasicConfigGui(@NotNull HumanEntity player, @NotNull Section config) {
+        super(player, config);
+    }
+
+    @Override
+    public Gui createGui() {
+        return Gui.gui()
+            .disableAllInteractions()
+            .title(getTitle().getComponentMessage())
+            .rows(getRows())
+            .type(getGuiType())
+            .create();
+    }
+
+}
