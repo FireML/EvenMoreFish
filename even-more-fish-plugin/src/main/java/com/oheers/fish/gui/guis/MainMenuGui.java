9222a56e8e20609a7876a798ffdb7674d0ade5d3
FireML <fireml@firedev.uk>
2025-09-21 21:42:34 +0100

    Update MainMenuGui

--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/guis/MainMenuGui.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/guis/MainMenuGui.java
@@ -1,18 +1,21 @@
 package com.oheers.fish.gui.guis;
 
 import com.oheers.fish.config.GuiConfig;
+import com.oheers.fish.exceptions.InvalidGuiException;
 import com.oheers.fish.gui.ConfigGuiOld;
+import com.oheers.fish.gui.types.BasicConfigGui;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class MainMenuGui extends ConfigGuiOld {
+public class MainMenuGui extends BasicConfigGui {
 
-    public MainMenuGui(@NotNull HumanEntity viewer) {
+    public MainMenuGui(@NotNull Player viewer) throws InvalidGuiException {
         super(
-            GuiConfig.getInstance().getConfig().getSection("main-menu"),
-            viewer
+            viewer,
+            GuiConfig.getInstance().getConfig().getSection("main-menu")
         );
-        createGui();
+        init();
     }
 
 }
