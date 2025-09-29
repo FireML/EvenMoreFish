efc735499fd5c67804f4e797aa0cced9f6b57555
FireML <fireml@firedev.uk>
2025-09-29 19:57:32 +0100

    Fix broken page buttons

--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/config/GuiConversions.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/config/GuiConversions.java
@@ -5,7 +5,9 @@ import com.oheers.fish.api.Logging;
 import com.oheers.fish.config.ConfigBase;
 import com.oheers.fish.config.ConfigUtils;
 import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.block.Comments;
 import dev.dejvokep.boostedyaml.block.implementation.Section;
+import dev.dejvokep.boostedyaml.utils.format.NodeRole;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -14,34 +16,53 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// TODO map character and item to row/column. Basic math but i'm dumb
+// TODO map filler and general items to the correct areas, and convert the filler key to the new section.
 public class GuiConversions {
 
-    private final String guiYml = "test.yml";
-    private final String guiFillerYml = "gui-fillers.yml";
+    private final String guiYml = "guis.yml";
+    private final ConfigBase gui = resolveConfigBase(guiYml);
+    private final ConfigBase guiFiller = resolveConfigBase("gui-fillers.yml");
+    private final Section generalSection;
+
+    public GuiConversions() {
+        guiFiller.getConfig().remove("version");
+        generalSection = ConfigUtils.getOrCreateSection(gui.getConfig(), "general");
+        if (generalSection.contains("first-page")) {
+            generalSection.set("first-page.character", "f");
+        }
+        if (generalSection.contains("previous-page")) {
+            generalSection.set("previous-page.character", "p");
+        }
+        if (generalSection.contains("next-page")) {
+            generalSection.set("next-page.character", "n");
+        }
+        if (generalSection.contains("last-page")) {
+            generalSection.set("last-page.character", "l");
+        }
+    }
 
     public void performCheck() {
         // TODO aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         if (!new File(EvenMoreFish.getInstance().getDataFolder(), this.guiYml).exists()) {
             return;
         }
-        ConfigBase guiYml = resolveConfigBase(this.guiYml);
-        ConfigBase fillerYml = resolveConfigBase(this.guiFillerYml);
-        Section generalSection = ConfigUtils.getOrCreateSection(guiYml.getConfig(), "general");
 
-        handleSection(guiYml, "main-menu", "guis/main-menu.yml");
-        handleSection(guiYml, "sell-menu-normal", "guis/sell-normal.yml");
-        handleSection(guiYml, "sell-menu-confirm", "guis/sell-confirm.yml");
-        handleSection(guiYml, "baits-menu", "guis/baits.yml");
-        handleSection(guiYml, "apply-baits-menu", "guis/apply-baits.yml");
-        handleSection(guiYml, "journal-menu", "guis/journal-main.yml");
-        handleSection(guiYml, "journal-rarity", "guis/journal-rarity.yml");
+        handleSection(gui, "main-menu", "guis/main-menu.yml");
+        handleSection(gui, "sell-menu-normal", "guis/sell-normal.yml");
+        handleSection(gui, "sell-menu-confirm", "guis/sell-confirm.yml");
+        handleSection(gui, "baits-menu", "guis/baits.yml");
+        handleSection(gui, "apply-baits-menu", "guis/apply-baits.yml");
+        handleSection(gui, "journal-menu", "guis/journal-main.yml");
+        handleSection(gui, "journal-rarity", "guis/journal-rarity.yml");
+
+        finalizeConversion();
     }
 
     private void handleSection(@NotNull ConfigBase guiYml, @NotNull String sectionName, @NotNull String fileName) {
         Section section = guiYml.getConfig().getSection(sectionName);
         if (section != null) {
             Map<Character, List<String>> mappedChars = mapCharacterToSlots(section.getStringList("layout"));
+            section.remove("layout");
             ConfigBase base = resolveConfigBase(fileName);
             mapSectionToFile(section, base, mappedChars);
         }
@@ -55,17 +76,23 @@ public class GuiConversions {
         return new File(EvenMoreFish.getInstance().getDataFolder(), name);
     }
 
-    private void finalizeConversion(@NotNull ConfigBase guisConfig) {
+    private void finalizeConversion() {
         // Rename the file to guis.yml.old
-        File file = guisConfig.getFile();
+        File file = gui.getFile();
         file.renameTo(new File(EvenMoreFish.getInstance().getDataFolder(), guiYml + ".old"));
         file.delete();
+
+        File fillerFile = guiFiller.getFile();
+        fillerFile.renameTo(new File(EvenMoreFish.getInstance().getDataFolder(), "gui-fillers.yml.old"));
+        fillerFile.delete();
 
         Logging.info("<yellow>Your GUI configs have been automatically converted to the new format.");
     }
 
     private void mapSectionToFile(@NotNull Section section, @NotNull ConfigBase file, @NotNull Map<Character, List<String>> mappedChars) {
         YamlDocument config = file.getConfig();
+        config.setAll(guiFiller.getConfig().getRouteMappedValues(true));
+        config.setAll(generalSection.getRouteMappedValues(true));
         config.setAll(section.getRouteMappedValues(true));
 
         for (String sectionName : config.getRoutesAsStrings(false)) {
@@ -83,12 +110,17 @@ public class GuiConversions {
     private void updateItemFormat(@NotNull Section itemSection, @NotNull Map<Character, List<String>> mappedChars) {
         Character character = itemSection.getChar("character");
         if (character == null || !mappedChars.containsKey(character)) {
-            // Wipes the section.
-            itemSection.clear();
+            // Removes the invalid item from the config (please work)
+            itemSection.getParent().remove(itemSection.getNameAsRoute());
             return;
         }
         itemSection.remove("character");
-        itemSection.set("locations", mappedChars.getOrDefault(character, List.of()));
+        List<String> locations = mappedChars.get(character);
+        if (locations == null || locations.isEmpty()) {
+            itemSection.getParent().remove(itemSection.getNameAsRoute());
+            return;
+        }
+        itemSection.set("locations", locations);
     }
 
     // This could maybe be optimized? But it does work as intended, so I cannot complain.
