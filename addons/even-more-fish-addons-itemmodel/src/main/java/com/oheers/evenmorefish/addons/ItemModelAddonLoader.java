1c9cb7fa1aa2cde723b44e3aa27e451dbb9cb3cd
FireML <fireml@firedev.uk>
2025-11-30 02:17:45 +0000

    Add MinecraftVersionHelper to avoid shading issues

--- addons/even-more-fish-addons-itemmodel/src/main/java/com/oheers/evenmorefish/addons/ItemModelAddonLoader.java
+++ addons/even-more-fish-addons-itemmodel/src/main/java/com/oheers/evenmorefish/addons/ItemModelAddonLoader.java
@@ -5,7 +5,9 @@ import com.oheers.fish.api.plugin.EMFPlugin;
 import com.oheers.fish.api.utils.system.JavaSpecVersion;
 import com.oheers.fish.api.utils.system.SystemUtils;
 import com.oheers.fish.items.ItemConfigResolver;
+import com.oheers.fish.utils.MinecraftVersionHelper;
 import de.tr7zw.changeme.nbtapi.utils.MinecraftVersion;
+import org.bukkit.Bukkit;
 
 import java.io.File;
 
@@ -18,7 +20,7 @@ public class ItemModelAddonLoader extends AddonLoader {
 
     @Override
     public boolean canLoad() {
-        return SystemUtils.isJavaVersionAtLeast(JavaSpecVersion.JAVA_21) && MinecraftVersion.isAtLeastVersion(MinecraftVersion.MC1_21_R2);
+        return SystemUtils.isJavaVersionAtLeast(JavaSpecVersion.JAVA_21) && MinecraftVersionHelper.isAtLeastVersion("MC1_21_R2");
     }
 
     @Override
